daf8736688a52df94286e61beae7aaa480242b55
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-02-09 12:04:38 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Amir Kabiri on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
