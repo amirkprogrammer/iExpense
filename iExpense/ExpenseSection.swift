7527e199630345db445585763425ad03fd4bbc49
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-02-29 13:28:47 -0800

    - add AddView, Expense Section, and View-Expense Style

---
+++ iExpense/ExpenseSection.swift
@@ -0,0 +1,42 @@
+//
+//  ExpenseSection.swift
+//  iExpense
+//
+//  Created by Amir Kabiri on 2/18/24.
+//
+
+import SwiftUI
+
+struct ExpenseSection: View {
+    let title: String
+    let expenses: [ExpenseItem]
+    let deleteItems: (IndexSet) -> Void
+    
+    let localCurrency = Locale.current.currency?.identifier ?? "USD"
+    
+    var body: some View {
+        Section(title) {
+            ForEach(expenses) { item in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(item.name)
+                            .font(.headline)
+                        Text(item.type)
+                    }
+                    
+                    Spacer()
+                    
+                    Text(item.amount, format: .currency(code: localCurrency))
+                        .style(for: item)
+                }
+            }
+            .onDelete(perform: deleteItems)
+        }
+    }
+}
+
+#Preview {
+    ExpenseSection(title: "Personal", expenses: []) { _ in
+        
+    }
+}
---
+++ iExpense/View-ExpenseStyle.swift
@@ -0,0 +1,20 @@
+//
+//  View-ExpenseStyle.swift
+//  iExpense
+//
+//  Created by Amir Kabiri on 2/18/24.
+//
+
+import SwiftUI
+
+extension View {
+    func style(for item: ExpenseItem) -> some View {
+        if item.amount < 10 {
+            return self.font(.body)
+        } else if item.amount < 100 {
+            return self.font(.title3)
+        } else {
+            return self.font(.title)
+        }
+    }
+}
